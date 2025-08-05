fac66a495325fd40eba5b7dc9ff3b2a0b165e1d5
annapiro <26202085+annapiro@users.noreply.github.com>
2025-08-05 15:18:34 +0200

    basic inference setup

--- inference.py
+++ inference.py
@@ -12,11 +12,11 @@ from transformers import AutoModelForCausalLM, BitsAndBytesConfig, pipeline
 load_dotenv()
 model_name = os.getenv('MODEL_NAME')
 
+
 quantization_config = BitsAndBytesConfig(
     load_in_4bit=True,  # Enable 4-bit inference
     bnb_4bit_compute_dtype="bfloat16"
 )
-
 
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
@@ -25,6 +25,7 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map="auto",
     #attn_implementation="flash_attention_2",
 )
+
 tokenizer = transformers.AutoTokenizer.from_pretrained(
     model_name,
     padding_side="left",
@@ -38,15 +39,12 @@ generator = pipeline(
         "text-generation",
         model=model,
         tokenizer=tokenizer,
-        )
+)
 
 while True:
-
-    print("\n\n\n")
-    prompt = input("Prompt: ")
-    
-    out = generator(prompt, max_length=50)
-    print(out)
+    prompt = input("\nPrompt: ").replace('\\n', '\n')
+    out = generator(prompt)
+    print(out[0]['generated_text'])
     #inputs = tokenizer.encode(prompt, return_tensors="pt")  # Get tensors
     #input_ids = inputs
     #attention_mask = torch.ones_like(input_ids, dtype=torch.long)
@@ -67,4 +65,3 @@ while True:
 
     #print(output)
     #print(tokenizer.decode(output), end="", flush=True)
-
